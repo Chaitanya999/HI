fa70c0f51e6d9c4f576129d9aa56b87c380045e1
Student <student@achiever27.local>
2016-09-29 11:54:39 +0530

    gg

---
+++ hiii/hiii/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  hiii
+//
+//  Created by Student on 29/09/16.
+//  Copyright © 2016 brn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ hiii/hiii/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  hiii
+//
+//  Created by Student on 29/09/16.
+//  Copyright © 2016 brn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
